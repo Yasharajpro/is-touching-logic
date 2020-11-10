512010a7dbe507517fc924d9967025fcc23b3b57
Yasharajpro <73935946+Yasharajpro@users.noreply.github.com>
2020-11-10 15:44:04 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var fixedRect, movingRect; 
+function setup() {
+   createCanvas(800,400);
+   fixedRect=createSprite(400, 200, 80, 50);
+    fixedRect.shapeColor="green"; 
+    movingRect=createSprite(100,100,50,80); 
+    movingRect.shapeColor="green"; } 
+function draw() {
+   background("black");
+ movingRect.x=mouseX;
+  movingRect.y=mouseY; 
+  if(movingRect.x-fixedRect.x<movingRect.width/2+fixedRect.width/2 && 
+    fixedRect.x-movingRect.x<movingRect.width/2+fixedRect.width/2 &&
+    movingRect.y-fixedRect.y<movingRect.height/2+fixedRect.height/2 &&
+    fixedRect.y-movingRect.y<movingRect.height/2+fixedRect.height/2 ){
+     fixedRect.shapeColor="red"; movingRect.shapeColor="red"; } 
+     else {
+        movingRect.shapeColor="green"; 
+     fixedRect.shapeColor="green"; } 
+     drawSprites(); }
